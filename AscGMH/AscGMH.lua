12b438c761e2cc24ffb92965db2bf49545a53301
DankoDJ <matzesper@aol.com>
2016-05-17 18:36:29 +0200

    Toggle GMH mainwindow (open/close). Show button near minimap. Move minimap button and mainwindow with right click.

---
+++ AscGMH/AscGMH.lua
@@ -0,0 +1,24 @@
+﻿--[[
+ AscGMH - Powered by AscEmu-Community
+ Copyright (c) 2014-2016 AscEmu Team <http://www.ascemu.org/>
+ This file is released under the MIT license.
+]]
+
+---------------------Not AddOn related------------------------
+SLASH_RELOADUI1 = "/rl";
+SlashCmdList.RELOADUI = ReloadUI;
+--------------------------------------------------------------
+
+-- open/close AscEmu GMH MainForm
+function ToggleGMH()
+   if( is_open == true )
+   then
+      MainForm:Hide();
+      PlaySound("INTERFACESOUND_CHARWINDOWCLOSE");
+      is_open = false;
+   else
+      MainForm:Show();
+      PlaySound("INTERFACESOUND_CHARWINDOWOPEN");
+      is_open = true;
+   end
+end
